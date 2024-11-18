4de7c98fc5446b8eba90f26a5ff119073d0dc14e
sbonnin3 <simonbonnin95@gmail.com>
2024-11-18 21:44:57 +0100

    Fix publicPath for Vercel deployment

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/flip/'
-    : '/',
-    productionSourceMap: false,
-    configureWebpack: {
-      performance: {
-        hints: false
-      }
-    },
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  }
+});
